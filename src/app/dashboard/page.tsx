e959a0d39af0ba315de3e9e864fb9b7471cb8aa6
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2025-01-19 14:18:54 -0500

    working camera component without upload capability

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,20 +4,16 @@ import { Button } from "@/components/ui/button";
 import { checkImage } from "@/server/actions";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
+import Webcam from "react-webcam";
 
-// import { useCallback, useEffect, useRef, useState } from "react";
-
-// import { User } from "@supabase/supabase-js";
-// import { useRouter } from "next/navigation";
-// import { TopNav } from "../_components/topnav";
+import { TopNav } from "../_components/topnav";
 // import * as rdd from 'react-device-detect';
-// import Webcam from "react-webcam";
-// import { redirect } from "next/navigation";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
+
+import { Input } from "@/components/ui/input";
 // // import { updateImage } from "./actions";
-// import Image from "next/image";
+import Image from "next/image";
+import { Session } from "next-auth";
 
 // function CameraComponent({user, onImageUpload}: {user: User, onImageUpload: () => void}) {
 //     const isMobile = window.innerWidth < 768;
@@ -29,21 +25,6 @@ import { useCallback, useEffect, useState } from "react";
 //       height: height,
 //       facingMode: "user",
 //     };
-
-//     const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
-//     const webcamRef = useRef<Webcam>(null);
-//     const [url, setUrl] = useState<string | null>(null);
-
-//     const capture = useCallback(() => {
-//       const imageSrc = webcamRef.current?.getScreenshot({width: isMobile ? height : width, height: isMobile ? width : height});
-//       if (imageSrc) {
-//         setUrl(imageSrc);
-//         console.log(imageSrc);
-
-//         setCaptureEnable(false);
-//         console.log(imageSrc);
-//       }
-//     }, [webcamRef, setUrl]);
 
 //     return (
 //         <div className="flex flex-col items-center justify-center gap-4">
@@ -146,6 +127,85 @@ import { useCallback, useEffect, useState } from "react";
 
 // }
 
+function CameraComponent({session}: {session: Session | null}) {
+  const isMobile = window.innerWidth < 768;
+  const width = 300;
+  const height = 400;
+  console.log("mobile: ", isMobile);
+  const videoConstraints = {
+    width: width,
+    height: height,
+    facingMode: "user",
+  };
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot({
+      width: width,
+      height: height,
+    });
+    if (imageSrc) {
+      setUrl(imageSrc);
+      console.log(imageSrc);
+
+      setCaptureEnable(false);
+      console.log(imageSrc);
+    }
+  }, [webcamRef, setUrl]);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+        <div>
+            Welcome, {session?.user?.firstname} {session?.user?.lastname}
+        </div>
+        {!url && (
+            <>
+                <div className="flex flex-row gap-4">
+                    <Button onClick={() => {
+                        if (isCaptureEnable) {
+                            setCaptureEnable(false);
+                            setUrl(null);
+                        }
+                        else {
+                            setCaptureEnable(true);
+                        }
+                    }}>{isCaptureEnable ? "Close Camera" : "Open Camera"} </Button>
+                    {isCaptureEnable && <Button onClick={capture}>Capture</Button>}
+                </div>
+            </>
+        )}
+        {isCaptureEnable && (
+            <>
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                    mirrored={true}
+                />
+            </>
+        )}
+        {url && (
+            <>
+                <div className="flex flex-row justify-center w-full gap-4">
+                    <Button onClick={() => {setUrl(null)}} className="w-fit">Delete</Button>
+                    <form action={async (formData) => {
+                        
+                    }}>
+                        <input name="image" defaultValue={url} hidden />
+                        <Button className="w-fit" type="submit">Upload Image</Button>
+                    </form>
+                </div>
+                <img src={url} alt="captured" />
+            </>
+        )}
+    </div>
+  );
+}
+
 export default function Dashboard() {
   const { data: session, status } = useSession();
   const [imageStatus, setImageStatus] = useState<boolean>(false);
@@ -153,39 +213,31 @@ export default function Dashboard() {
 
   useEffect(() => {
     const getImageStatus = async () => {
-        if (session?.user?.id) {
-          const result = await checkImage(session.user.id);
-          setImageStatus(result);
-        }
-    } 
+      if (session?.user?.id) {
+        const result = await checkImage(session.user.id);
+        setImageStatus(result);
+      }
+    };
     getImageStatus();
   }, [session?.user.image]);
 
-
   if (status === "loading") {
     return (
-        <div>
-            <p>Loading...</p>
-        </div>
+      <div>
+        <p>Loading...</p>
+      </div>
     );
   }
   if (!session) {
-    router.push("/sign-up")
+    router.push("/sign-up");
   }
 
   return (
-    <div>
-      Dashboard 
-      {session?.user?.firstname} {session?.user?.lastname}{" "}
-      {session?.user?.phone}
-      {imageStatus ? "Image Uploaded" : "Image Not Uploaded"}
-      <Button
-        onClick={() => {
-          signOut();
-        }}
-      >
-        Sign Out
-      </Button>
+<div>
+    <TopNav />
+    <div className="min-w-screen flex flex-col gap-4 justify-center items-center h-full mt-10">
+        <CameraComponent session={session} />
     </div>
+</div>
   );
 }
